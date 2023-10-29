e90b5375dfeb43e0ae6d8e370d67443d622d78be
Matias Luna <mtsluna@gmail.com>
2023-10-29 10:59:11 -0300

    feat: add mock

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
 import express from 'express';
 import 'dotenv/config';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import cors from 'cors';
 import router from './src/routes';
-import { AppDataSource } from './src/configurations/typeorm/data-source';
-import { Restaurant } from './src/entities/restaurant';
 
 const app = express();
+
+app.use(cors());
 
 app.use(router);
 
 app.listen(process.env.PORT, async () => {
   console.log('Server started');
 
-  await AppDataSource.initialize();
+  // await AppDataSource.initialize();
 
-  const restaurant = new Restaurant();
-  restaurant.name = 'Mc Donalds';
-
-  await Restaurant.getRepository().save(restaurant);
+  // const restaurant = new Restaurant();
+  // restaurant.name = 'Mc Donalds';
+  //
+  // await Restaurant.getRepository().save(restaurant);
 });
